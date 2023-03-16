9acf33f624103f28fe6a90bddecf32a203648808
jyoung7988 <jyoung7988@naver.com>
2023-03-16 17:59:41 +0900

    State 사용 data를 store로 이동시켜서 state로 접근할 수 있게 이동

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,24 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+const storage = {
+    fetch(){
+        const arr =[];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        } 
+        return arr;
+    }
+}
+
 export const store = new Vuex.Store({
     //store를 외부에서 사용할 수 있다. main.js 로
+    state:{
+        todoItems: storage.fetch() // store에서 todoItems 를 가져와서 todoList에 뿌려준다.
+    }
 });
 
