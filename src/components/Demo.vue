0a3181ffba9f14bb29af9c6690043773baa7c076
jyoung7988 <jyoung7988@naver.com>
2023-04-03 11:01:44 +0900

    헬퍼함수의 역할 computed 메소드에서 getters 를 가져오는 코드를 줄여준다

---
+++ src/components/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="root">
+    <p>{{originalPrice}}</p>  <!--100-->
+    <p>{{doublePrice}}</p>  <!--200-->
+    <p></p>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+    computed:{
+        ...mapGetters(['originalPrice','doublePrice'])
+
+        // originalPrice() {
+        //     return this.$store.getters.originalPrice
+        // },
+        // doublePrice() {
+        //     return this.$store.getters.doublePrice
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
